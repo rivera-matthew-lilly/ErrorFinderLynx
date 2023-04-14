e6038be50ad36a022c3d7c63e48f03a0674bbe01
Matthew Rivera <rivera_matthew@lilly.com>
2023-04-14 15:04:12 -0700

    first commit - working to add to DB

---
+++ ErrorFinderLynx/FunctionBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace FunctionBase { 
+    public class FunctionBaseSet
+    {
+        public static Tuple<string, string, string, string, string, int> ParseInputString(string input, int primaryKey)
+        {
+            // Check that input string is at least 21 characters long
+            if (input.Length < 21)
+            {
+                throw new ArgumentException("Input string must be at least 21 characters long");
+
+            }
+
+            string date = input.Substring(0, 10);
+            string time = input.Substring(11, 10);
+            string type = string.Empty;
+            string methodName = string.Empty;
+            string errorText = string.Empty;
+
+
+            if (input.Length > 22)
+            {
+                if (input[22] == 'P')
+                {
+                    type = input.Substring(22, 7);
+                    int firstCommaIndex = input.IndexOf("'");
+                    int secondCommaIndex = input.IndexOf("'", firstCommaIndex + 1);
+                    if (firstCommaIndex != -1 && secondCommaIndex != -1)
+                    {
+                        methodName = input.Substring(firstCommaIndex + 1, secondCommaIndex - firstCommaIndex - 1);
+                        //Console.WriteLine(methodName);
+                        errorText = input.Substring(secondCommaIndex + 19);
+                        //Console.WriteLine(errorText);
+                        primaryKey++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input string format");
+                    }
+                }
+                else if (input[22] == 'M')
+                {
+                    type = input.Substring(22, 6);
+                    int firstCommaIndex = input.IndexOf("'");
+                    int secondCommaIndex = input.IndexOf("'", firstCommaIndex + 1);
+                    if (firstCommaIndex != -1 && secondCommaIndex != -1)
+                    {
+                        methodName = input.Substring(firstCommaIndex + 1, secondCommaIndex - firstCommaIndex - 1);
+                        Console.WriteLine(methodName);
+                        errorText = input.Substring(secondCommaIndex + 18);
+                        Console.WriteLine(errorText);
+                        primaryKey++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input string format");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input string format");
+                }
+            }
+
+
+
+
+            return Tuple.Create(date, time, type, methodName, errorText, primaryKey);
+        }
+
+
+        internal static void InsertRow(int primaryKey, string date, string timeStamp, string type, string method, string errorText)
+        {
+            string databaseName = "C:\\Matthew IC Copy For Test\\Database\\SystemHealthBase.db";
+            SQLiteConnection SystemHealthBase = new SQLiteConnection(@"data source = " + databaseName);
+            SystemHealthBase.Open();
+            string tableName = "LynxErrorLogInfo";
+            string commandString = "INSERT INTO " + tableName + " VALUES ('" + primaryKey.ToString() + "', '" + date + "', '" + timeStamp + "', '" + type + "', '" + method + "', '" + errorText + "');"; //@"INSERT INTO " + tableName + " VALUES (" + date + ", " + timeStamp + ", " + instrumentName + ", " + commandName + ", " + manufacture + ", " + errorText + ");";
+            SQLiteCommand command = new SQLiteCommand(commandString, SystemHealthBase);
+            Console.WriteLine(commandString);
+            command.ExecuteNonQuery();
+            SystemHealthBase.Close();
+            Console.WriteLine("Data Added Sucessfully");
+        }
+
+        public static string RemoveSingleQuote(string inputString)
+        {
+            string newString = string.Empty;
+            if (inputString.Contains("'"))
+            {
+                newString = inputString.Replace("'", " ");
+            }
+            else
+            {
+                newString = inputString;
+            }
+            return newString;
+        }
+
+    }
+ } 
---
+++ ErrorFinderLynx/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using FunctionBase;
+
+namespace ErrorFinderLynx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Matthew IC Copy For Test\Lynx\Output";
+            string searchWord1 = "error";
+            string searchWord2 = "Method";
+            int primaryKey = 10000;
+            //do not inculde dates before 07/11/2019
+            //02/16/2023 14:47:05.2 Process 'Production Methods\Fill Blocks with Media (SVJ).met' MethodCompleted, A 'Aspirate(VVP96)' command was stopped due to a motion error
+            //02/20/2023 08:19:22.0 Method 'Biologica DNA Miniprep Mixing V2,0 (MJR)' had an error in line 6, 'Load Tips(VVP96)'[Command 'Load Tips(VVP96)': Command 'Load Tips(VVP96)' can not have an existing tip type
+            //9 char {0-8}, 10 char {10-20}, if index 22 == P 7 char {22-28} elif index 22 == M 6 char {22-27},
+            foreach (string filePath in Directory.GetFiles(directoryPath))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string line;
+                        int lineNumber = 0;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            lineNumber++;
+                            if (line.Contains(searchWord1) && line.Contains(searchWord2))
+                            {
+                                var parsedLine = FunctionBaseSet.ParseInputString(line, primaryKey);
+                                Console.WriteLine("---------------------------------------------------------------------");
+                                Console.WriteLine(line);
+                                //Console.WriteLine("Date: " + parsedLine.Item1);
+                                //Console.WriteLine("Time: " + parsedLine.Item2);
+                                //Console.WriteLine("Type: " + parsedLine.Item3);
+                                //Console.WriteLine("Method: " + parsedLine.Item4);
+                                //Console.WriteLine("Error: " + parsedLine.Item5);
+                                //Console.WriteLine("Primary Key: " + parsedLine.Item6);
+
+                                string date = FunctionBaseSet.RemoveSingleQuote(parsedLine.Item1);
+                                //Console.WriteLine(date);
+                                string time = FunctionBaseSet.RemoveSingleQuote(parsedLine.Item2);
+                                string type = FunctionBaseSet.RemoveSingleQuote(parsedLine.Item3);
+                                string method = FunctionBaseSet.RemoveSingleQuote(parsedLine.Item4);
+                                string error = FunctionBaseSet.RemoveSingleQuote(parsedLine.Item5);
+                                int pKey = parsedLine.Item6;
+                                primaryKey = parsedLine.Item6;
+                                
+                                FunctionBaseSet.InsertRow(pKey, date, time, type, method, error);
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error reading file {0}: {1}", Path.GetFileName(filePath), e.Message);
+                }
+            }
+        }
+    }
+
+}
